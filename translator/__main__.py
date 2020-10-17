9e2133cb708d4be92edff31bb81f48304f732972
JDJFisher <jordanfisher2096@gmail.com>
2020-10-17 15:54:44 +0100

    ✨ Added file I/O

--- translator/__main__.py
+++ translator/__main__.py
@@ -2,19 +2,41 @@
 
 # Standard library imports
 import argparse
+import os
 
 
+# Program entrypoint
 def main(argv=None):
+  # Build arg parser
   parser = argparse.ArgumentParser(prog='translator')
   parser.add_argument('-version', '--version', help='Version', action='store_true')
   parser.add_argument('-v', '--verbose', help='Verbose', action='store_true')
+  parser.add_argument('-o', '--out', help='Output Directory', type=isDirPath, default='.')
+  parser.add_argument('in', help='Input Files', type=isFilePath, nargs='+')
   args = parser.parse_args(argv)
 
+  # Dump version number
   if args.version:
-    print('0.0.0')
+    print('v0.0.0')
     exit()
 
-  print('Hello world!')
+  # ...
+  print(args)
+  exit()
+
+
+def isDirPath(path):
+  if os.path.isdir(path):
+    return path
+  else:
+    raise argparse.ArgumentTypeError(f"invalid dir path: {path}")
+
+
+def isFilePath(path):
+  if os.path.isfile(path):
+    return path
+  else:
+    raise argparse.ArgumentTypeError(f"invalid file path: {path}")
 
 
 if __name__ == '__main__':
